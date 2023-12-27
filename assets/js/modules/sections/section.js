45ea19b833d446a480f46feba7334dcbfaa02ef7
sequenter <35529212+sequenter@users.noreply.github.com>
2023-12-27 19:01:46 +0000

    Updated section module

--- assets/js/modules/sections/section.js
+++ assets/js/modules/sections/section.js
@@ -10,15 +10,23 @@ export class Section {
         };
     }
 
+    //getAvailable
+    //Returns a sorted array of unlocked @data items and updates @progress counts
+    //@fUnlocked - callback function to obtain item unlock status
+    //@completed - completed localStorage object
+    //@fSort - sort comparison function
+    //@completedKey - key to use for the @completed object
     getAvailable(fUnlocked, completed, fSort, completedKey) {
         const available = [];
 
+        //Reset progress counts
         this.progress.complete = 0;
         this.progress.total = 0;
 
         for(const key of Object.keys(this.data)) {
             const obj = this.data[key];
 
+            //Add data item and update counts if unlocked
             if (fUnlocked(obj.requirements)) {
                 obj.active = completed.includes(obj[completedKey]);
                 available.push(obj);
@@ -31,18 +39,28 @@ export class Section {
         return available.sort(fSort);
     }
 
+    //onItemClick
+    //Toggles item active status
+    //@event - click event target
     onItemClick(event) {
         $(event.currentTarget).toggleClass('_active');
         $(event.currentTarget).toggleClass('_inactive');
 
+        //Item click callback with item id
         this.onClick($(event.currentTarget).find('._id').text());
     }
 
+    //updateProgress
+    //Updates progress elements with @progress counts
+    //@selectors - element CSS selectors to update
     updateProgress(selectors) {
+
+        //Update total, complete and incomplete progress counts
         $(selectors.total).text(this.progress.total);
         $(selectors.complete).text(this.progress.complete);
         $(selectors.incomplete).text(this.progress.total - this.progress.complete);
 
+        //Update progress bar width
         $(selectors.bar).css('width', `${(this.progress.complete / this.progress.total) * 100}%`);
     }
 }
